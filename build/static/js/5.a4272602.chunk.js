ddb9e3b1131fdd02e73c9d804bdd3cb29b61e39d
AIASolution <58142664+bestsolution4u@users.noreply.github.com>
2022-10-14 10:36:29 -0600

    add : all

--- build/static/js/5.a4272602.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[5],{744:function(n,o){},758:function(n,o){}}]);
-//# sourceMappingURL=5.a4272602.chunk.js.map
--- build/static/js/7.e6e379a8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[7],{708:function(n,o){}}]);
-//# sourceMappingURL=7.e6e379a8.chunk.js.map
